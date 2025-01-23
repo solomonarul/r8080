08ae56f9b4bdc48fbb729ed28f8fecd5fed2bcda
Solomonarul <solomonarul@tuta.io>
2025-01-23 21:34:35 +0200

    feat(CPU): It should work across multiple threads now.

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,6 +1,8 @@
 mod instruction;
 mod interpreter;
 
+use std::{any::Any, sync::{Arc, RwLock}};
+
 use crate::Bus8080;
 
 pub type Interpreter8080 = interpreter::Interpreter8080;
@@ -13,10 +15,14 @@ pub type InstructionType = instruction::InstructionTarget;
 pub type Condition = instruction::Condition;
 pub type RegisterFlags = instruction::RegisterFlags;
 
-pub trait CPU8080
+pub trait CPU8080: Any + Send + Sync
 {
     fn force_jump(&mut self, a: u16);
-    fn set_bus(&mut self, b: Box<dyn Bus8080>);
-    fn remove_bus(&mut self) -> Box<dyn Bus8080>;
+    fn set_bus(&mut self, b: Arc<RwLock<Box<dyn Bus8080>>>);
+    fn get_bus(&self) -> Arc<RwLock<Box<dyn Bus8080>>>;
+    fn remove_bus(&mut self) -> Arc<RwLock<Box<dyn Bus8080>>>;
+    fn stop(&mut self);
+    fn is_running(&mut self) -> bool;
+    fn step(&mut self);
     fn run(&mut self);
 }
